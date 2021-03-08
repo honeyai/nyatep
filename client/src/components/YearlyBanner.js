814c0d8246ef6b84dbb35710309fd092a73fb5db
Angel Juarez <angeljuarez729@gmail.com>
2021-03-08 13:49:55 -0500

    yearly banner start

---
+++ client/src/components/YearlyBanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function YearlyBanner() {
+  return (
+    <div className="yearly-banner">
+      <ul>
+        <li>2021</li>
+        <li>2020</li>
+        <li>2019</li>
+        <li>2018</li>
+      </ul>
+    </div>
+  )
+}
+
+export default YearlyBanner;
