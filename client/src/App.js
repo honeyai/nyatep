4c7583a2c99324aefad97bf0b184392e5db561d9
Angel Juarez <angeljuarez729@gmail.com>
2021-03-10 14:24:20 -0500

    slideshow fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import Slideshow from './components/Slideshow';
+import HeroSlideshow from './components/HeroSlideShow/HeroSlideshow';
 import YearlyInfo from './components/yearlyinfo/YearlyInfo';
-import YearlyBanner from './components/yearlyinfo/YearlyInfo';
 
 function App() {
   return (
     <div className="App">
-      <Slideshow />
+      <HeroSlideshow />
       <YearlyInfo />
     </div>
   );
