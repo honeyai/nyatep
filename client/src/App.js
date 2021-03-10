b06f203cbe6aeebf2e1784b94e002ec9a98cc192
Angel Juarez <angeljuarez729@gmail.com>
2021-03-10 14:04:26 -0500

    yearly info adjacent to slideshow

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Slideshow from './components/Slideshow';
-import YearlyBanner from './components/YearlyBanner';
+import YearlyInfo from './components/yearlyinfo/YearlyInfo';
+import YearlyBanner from './components/yearlyinfo/YearlyInfo';
 
 function App() {
   return (
     <div className="App">
       <Slideshow />
+      <YearlyInfo />
     </div>
   );
 }
