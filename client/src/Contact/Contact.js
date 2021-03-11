b58188fcfd3c271907b5cb4378cba16cbef01a7c
Jessica Sanchez <contact.jessicasanchez@gmail.com>
2021-03-11 13:27:23 -0500

    Need to fix the fact that it doesn't do each accordion

--- client/src/Contact/Contact.js
+++ client/src/Contact/Contact.js
@@ -4,10 +4,37 @@ import Accordion from "./Accordion/Accordion.js";
 import "./contact.css";
 
 const Contact = () => {
+
   return (
     <div>
       <div className="contact__regionAccordions">
-        <Accordion title="Testing" text={`${contactInfo[0].Region}`} />
+        {contactInfo.map((item, key) => {
+          return (
+            <Accordion
+              key={key}
+              title={item.Region}
+              text={item.contacts.map((item, key) => {
+                return (
+                  <div key={key}>
+                    <div>
+                      <div className="contact__contactinfo">
+                        <span className="contact__worksite">
+                          {item.site}
+                        </span>
+                        <span className="contact__nameAndEmail">
+                          {item.personToContact} {<br />} {item.email}
+                        </span>
+                        <span className="contact__phoneNumber">
+                          {item.phone}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            />
+          );
+        })}
       </div>
     </div>
   );
