33e6e054739ee3d113e32532f3c4a7db64477f2b
Jessica Sanchez <contact.jessicasanchez@gmail.com>
2021-03-13 13:04:32 -0500

    Added css global variables, motto banner and it's css almost done

---
+++ client/src/MottoBanner/MottoBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./mottoBanner.css";
+
+const MottoBanner = () => {
+  return (
+    <div>
+      <div className="banner__container">
+        <span className="banner__motto">Summer Youth Employment Matters</span>
+        <div className="banner__decorativeLine"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MottoBanner;
